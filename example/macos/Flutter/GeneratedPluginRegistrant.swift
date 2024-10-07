475b1d42d6fdd31474d6741fd99fe7876e0a8828
narciero <narciero@me.com>
2024-10-07 09:53:20 -0600

    update example app

--- example/macos/Flutter/GeneratedPluginRegistrant.swift
+++ example/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -10,7 +10,7 @@ import path_provider_foundation
 import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AnalyticsPlugin.register(with: registry.registrar(forPlugin: "AnalyticsPlugin"))
+  HightouchEventsPlugin.register(with: registry.registrar(forPlugin: "HightouchEventsPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
