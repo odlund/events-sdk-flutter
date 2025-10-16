04c1b0f235a098788f3d593b3b0855bec1a6ef3f
Anders Oedlund <aodlund@attaindata.io>
2025-10-16 10:28:55 -0700

    Update version

--- packages/core/lib/utils/lifecycle/fgbg.dart
+++ packages/core/lib/utils/lifecycle/fgbg.dart
@@ -10,9 +10,7 @@ class FGBGLifecycle extends LifeCycle {
   StreamSubscription<AppStatus> listen(void Function(AppStatus event)? onData,
       {Function? onError, void Function()? onDone, bool? cancelOnError}) {
     return _stream
-        .map((event) => (event == FGBGType.foreground)
-            ? AppStatus.foreground
-            : AppStatus.background)
+        .map((event) => (event == FGBGType.foreground) ? AppStatus.foreground : AppStatus.background)
         .listen(onData, onDone: onDone, onError: onError);
   }
 }
