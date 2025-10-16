0e9684e2bba0109be2940092fa251ecbb57fbe80
Anders Oedlund <aodlund@attaindata.io>
2025-10-16 10:07:39 -0700

    Update fgbg, timestamps are now UTC

--- packages/core/lib/utils/lifecycle/fgbg.dart
+++ packages/core/lib/utils/lifecycle/fgbg.dart
@@ -4,13 +4,15 @@ import 'package:hightouch_events/utils/lifecycle/lifecycle.dart';
 import 'package:flutter_fgbg/flutter_fgbg.dart';
 
 class FGBGLifecycle extends LifeCycle {
-  final _stream = FGBGEvents.stream;
+  final _stream = FGBGEvents.instance.stream;
 
   @override
   StreamSubscription<AppStatus> listen(void Function(AppStatus event)? onData,
       {Function? onError, void Function()? onDone, bool? cancelOnError}) {
     return _stream
-        .map((event) => (event == FGBGType.foreground) ? AppStatus.foreground : AppStatus.background)
+        .map((event) => (event == FGBGType.foreground)
+            ? AppStatus.foreground
+            : AppStatus.background)
         .listen(onData, onDone: onDone, onError: onError);
   }
 }
